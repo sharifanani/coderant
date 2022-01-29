05a50fc73bfc46f44cd4a00c55ee6255ecf0ef8f
Sharif Anani <sharif.anani@gmail.com>
2022-01-29 00:25:04 -0500

    Install microk8s & k9s
    When installing microk8s, run:
    microk8s config dump > ~/.kube/config
    to help k9s find the configuration easily
    
    When installing microk8s, enable:
    - traefik (listens on 8080)
    - registry (exposes registry on localhost:32000)
    
    You may need to include
    ```
    {
      "insecure-registries" : ["localhost:32000"]
    }
    ```
    in /etc/docker/daemon.json

--- examples/compose_to_skaffold/go/build-container.sh
+++ examples/compose_to_skaffold/go/build-container.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t coderant.dev/example_server:latest .
+docker build -t coderant.dev/example_server:latest -t localhost:32000/example_server:latest .
+docker push localhost:32000/example_server:latest
