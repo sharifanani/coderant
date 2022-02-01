919387a04a5c96705afabaa5f6d1cfe9ba96a4ae
Sharif Anani <sharif.anani@gmail.com>
2022-01-31 22:27:48 -0500

    Install podman from podman.io and switch the docker commands to use podman

--- examples/compose_to_skaffold/go/build-container.sh
+++ examples/compose_to_skaffold/go/build-container.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t coderant.dev/example_server:latest -t localhost:32000/example_server:latest .
-docker push localhost:32000/example_server:latest
+podman build -t coderant.dev/example_server:latest -t localhost:32000/example_server:latest .
+podman push localhost:32000/example_server:latest
